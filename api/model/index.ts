4c8a765f370a1ec76b7413d7bffba56c8d5d15e8
Tiputo <parekhonza@gmail.com>
2022-05-16 13:09:48 +0200

    initial

---
+++ api/model/index.ts
@@ -0,0 +1,13 @@
+export * from './Article'
+export * from './Button'
+export * from './ContactMessage'
+export * from './Content'
+export * from './ContentBlock'
+export * from './Image'
+export * from './Link'
+export * from './Menu'
+export * from './Page'
+export * from './Seo'
+export * from './Setting'
+
+
