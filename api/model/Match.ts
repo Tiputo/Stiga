a1f9fd6702542b4394dade254cd34024ccd92ce6
Tiputo <parekhonza@gmail.com>
2022-05-19 15:38:12 +0200

    Player home and away added relation

--- api/model/Match.ts
+++ api/model/Match.ts
@@ -2,11 +2,14 @@ import { SchemaDefinition as def } from "@contember/schema-definition"
 import { Player } from "./Player"
 
 export class Match {
-    playerHome = def.manyHasOne(MatchTeam)
-    playerAway = def.manyHasOne(MatchTeam)
+    playerHome = def.manyHasOne(MatchTeam, "matchPlayerHome")
+    playerAway = def.manyHasOne(MatchTeam, "matchPlayerAway")
 }
 
 export class MatchTeam {
+    matchPlayerHome = def.oneHasMany(Match, "playerHome")
+    matchPlayerAway = def.oneHasMany(Match, "playerAway")
+
     player = def.manyHasOne(Player, "teams").notNull().cascadeOnDelete()
     score = def.intColumn()
 }
