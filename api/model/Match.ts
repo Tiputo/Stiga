ac0834f7e5a6f9d24e327505054fb8b8f60352a5
Tiputo <parekhonza@gmail.com>
2022-05-20 12:15:44 +0200

    Modified Match to .cascadeOnDelete()

--- api/model/Match.ts
+++ api/model/Match.ts
@@ -2,13 +2,13 @@ import { SchemaDefinition as def } from "@contember/schema-definition"
 import { Player } from "./Player"
 
 export class Match {
-    playerHome = def.manyHasOne(MatchTeam, "matchPlayerHome")
-    playerAway = def.manyHasOne(MatchTeam, "matchPlayerAway")
+    playerHome = def.oneHasOneInverse(MatchTeam, "matchPlayerHome")
+    playerAway = def.oneHasOneInverse(MatchTeam, "matchPlayerAway")
 }
 
 export class MatchTeam {
-    matchPlayerHome = def.oneHasMany(Match, "playerHome")
-    matchPlayerAway = def.oneHasMany(Match, "playerAway")
+    matchPlayerHome = def.oneHasOne(Match, "playerHome").cascadeOnDelete()
+    matchPlayerAway = def.oneHasOne(Match, "playerAway").cascadeOnDelete()
 
     player = def.manyHasOne(Player, "teams").notNull().cascadeOnDelete()
     score = def.intColumn()
